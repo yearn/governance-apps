6a2b0a4336c9639f68f6c7395f4c74a328b41f5a
0xPickles <stabbytabby@tutanota.com>
2026-01-22 17:11:08 +0100

    chore: set up open-next and prep for cloudflare

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,38 @@
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 import type { NextConfig } from "next";
 
+initOpenNextCloudflareForDev();
+
+interface WebpackConfig {
+  resolve?: {
+    fallback?: Record<string, boolean>;
+    alias?: Record<string, boolean>;
+  };
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+
+  webpack: (config: WebpackConfig, { isServer }: { isServer: boolean }) => {
+    if (!isServer) {
+      config.resolve = config.resolve || {};
+
+      config.resolve.fallback = {
+        ...(config.resolve.fallback || {}),
+        fs: false,
+        net: false,
+        tls: false,
+      };
+
+      config.resolve.alias = {
+        ...(config.resolve.alias || {}),
+        "pino-pretty": false,
+        lokijs: false,
+        encoding: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
